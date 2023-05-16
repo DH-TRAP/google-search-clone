895d12fa2fe89983a305b51cb89dc29fc18c24ee
PARTH DOBARIYA <dobariya1212@gmail.com>
2023-05-16 10:06:58 +0530

    Updates

---
+++ precache-manifest.ab18acf79afbb88fe4a2ae2f54f32a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842d726389b8555493df819e21bd782f",
+    "url": "/google-search-clone/index.html"
+  },
+  {
+    "revision": "9d971d5703c8b3d1058d",
+    "url": "/google-search-clone/static/css/main.3e4a65ac.chunk.css"
+  },
+  {
+    "revision": "a5c9f658072d7d1eb7d2",
+    "url": "/google-search-clone/static/js/2.1c47119a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-search-clone/static/js/2.1c47119a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d971d5703c8b3d1058d",
+    "url": "/google-search-clone/static/js/main.7f3c6812.chunk.js"
+  },
+  {
+    "revision": "1da056fdad8874ce8138",
+    "url": "/google-search-clone/static/js/runtime-main.6935541d.js"
+  }
+]);
